f30a9d42841c59b17da73cbeeef9756de2d11d6d
Panditax0902 <juanchos2905@gmail.com>
2022-07-18 20:22:37 -0500

    clase-1

---
+++ fundamentos/app.js
@@ -0,0 +1,28 @@
+//Imprime un 'hola mundo por consola
+
+/*La siguiente linea
+me imprime un hola mundo por consola */
+
+//console.log('Hola Mundo')
+
+//console.log(b);
+
+let a = 10,
+  b = 20,
+  c = 30;
+  console.log(a)
+  console.log(b)
+  console.log(c)
+
+  console.log('')
+  
+  console.log("a =",a)
+  console.log("b =",b)
+  console.log("c =",c)
+  
+  console.log('')
+
+  console.log({ a })
+  console.log({ b })
+  console.log({ c })
+  
